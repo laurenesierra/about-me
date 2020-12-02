1ecc026b7ac6448b046caf755412843d91653b57
Lauren Sierra <laurenesierra@gmail.com>
2020-12-01 19:06:34 -0800

    added html css and javascript to about me page

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# about-me
+
+
+# LAB - 2
+
+
+## about-me
+
+I built an about me web page that opens with a quiz promt for the user.
+
+### Author: Student/Group Name
+Lauren Sierra
+
+### Links and Resources
+* [submission PR](http://xyz.com)
+* Any Links you used as reference
+https://www.w3schools.com/w3css/default.asp
+
+### Reflections and Comments
+* Consider including the answers to your daily journal and submission questions here
+* This is also a good place to reflect on the tools and resources used and learned
+
+### Collaborators
+Glenn Clark
---
+++ js/app.js
@@ -0,0 +1,58 @@
+// 'use strict';
+
+
+// //prompt for username
+// var userName = prompt('Hi! What\'s your name?').toLowerCase();
+// //console.log('The user name is ' + userName);
+
+// //Greeting
+// alert('Welcome ' + userName + ', to my "About Me" page. Would you like to take a quick quiz?');
+
+// //1st question
+// var residence = prompt('Do I live in the PNW? Yes or No?').toLowerCase();
+// //console.log(residence);
+// if (residence === 'yes' || residence === 'y') {
+// alert('That\'s correct! Nice job!');
+// } else {
+//   alert('Sorry, wrong answer!');
+// }
+
+// //2nd question
+// var favoriteColor = prompt('Is pink one of my favorite colors? Yes or No?').toLowerCase();
+// //console.log(favoriteColor);
+// if (favoriteColor === 'yes' || favoriteColor === 'y') {
+// alert('You got it!');
+// } else {
+//   alert('Sorry, wrong answer!');
+// }
+
+// //3rd question
+// var hiddenTalent = prompt('Can I whistle? Yes or No?').toLowerCase();
+// //console.log(hiddenTalent);
+// if (hiddenTalent === 'yes' || hiddenTalent === 'y') {
+// alert('You guessed it!');
+// } else {
+//   alert('Sorry, wrong answer!');
+// }
+
+// //4th question
+// var instrument = prompt('Did I play the violin growing up? Yes or No?').toLowerCase();
+// //console.log(instrument);
+// if (instrument === 'yes' || instrument === 'y') {
+// alert('Correct! Just one more!');
+// } else {
+//   alert('Sorry, wrong answer!');
+// }
+
+// //5th question
+
+// var teaDrinker = prompt('Am I a tea drinker? Yes or No?').toLowerCase();
+// //console.log(teaDrinker);
+// if (teaDrinker === 'yes' || teaDrinker === 'y') {
+// alert('Awesome job!');
+// } else {
+//   alert('Sorry, wrong answer!');
+// }
+
+// //final message
+//  alert('Way to go ' + userName + '!');
